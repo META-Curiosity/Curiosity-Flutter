9feb71e182d23316266188bf989cdf80d9fc6203
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2021-02-05 20:51:42 -0800

    Make good morning page const

--- lib/screens/good_morning_page.dart
+++ lib/screens/good_morning_page.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/material.dart';
 
 class GoodMorningPage extends StatelessWidget {
+  const GoodMorningPage({Key key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Center(
         child: Container(
-      padding:
-          EdgeInsets.only(top: 160.0, left: 30.0, right: 30.0, bottom: 30.0),
+      padding: const EdgeInsets.only(top: 160, left: 30, right: 30, bottom: 30),
       child: Column(
-        children: <Widget>[
+        children: const <Widget>[
           Text(
-            "Good morning",
-            style: TextStyle(fontSize: 36.0, fontWeight: FontWeight.bold),
+            'Good morning',
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
           ),
           SizedBox(height: 12),
           Text(
             "It's time to set your goals for today.",
-            style: TextStyle(fontSize: 16.0),
+            style: TextStyle(fontSize: 16),
           ),
         ],
       ),
