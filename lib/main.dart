4d514418efdf477cd99889778704b89150c15a10
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2021-04-22 13:43:27 -0700

    WIP changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,20 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-// import 'package:firebase_analytics/firebase_analytics.dart';
-// import 'package:firebase_analytics/observer.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
 import 'package:firebase_performance/firebase_performance.dart'; // actually used as a side-effect; don't delete!
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
 import 'controllers/navigation_controller.dart';
 import 'screens/play_audio_screen.dart';
 
-// FirebaseAnalytics analytics;
+FirebaseAnalytics analytics;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  // analytics = FirebaseAnalytics();
+  analytics = FirebaseAnalytics();
   FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
   runApp(
     // For widgets to be able to read providers, we need to wrap the entire
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'META Curiosity',
       initialRoute: '/',
-      // navigatorObservers: [FirebaseAnalyticsObserver(analytics: analytics)],
+      navigatorObservers: [FirebaseAnalyticsObserver(analytics: analytics)],
       routes: {
         // When navigating to the "/" route, build the FirstScreen widget.
         '/': (context) => const NavigationBarScreen(),
